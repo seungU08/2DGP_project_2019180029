f0e06fd496a113b499876bafdc82307c51aac303
SWnotebook <sungwoo5957@naver.com>
2023-10-27 14:43:01 +0900

    게임 오브젝트들 class 구현 Sky 구현중

---
+++ pikachu_map_objects.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+sprite_image = load_image('sprite_sheet.png')
+
+class Sky:
+    def __init__(self):
+        sky_x = 0
+        sky_y = 0
+
+    def draw(self):
+        sprite_image.draw()
+
+    def update(self):
+        pass
+
+class
+
