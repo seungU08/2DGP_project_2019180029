37780596b427204fda623b1582a249ac9d593924
SWnotebook <sungwoo5957@naver.com>
2023-10-27 14:42:14 +0900

    게임루프

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
+
 from pico2d import *
 
+import pikachu_world
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+def create_world():
+    global running
+
+    running = True
+
+
+def update_world():
+    pikachu_world.update()
+
+def render_world():
+    clear_canvas()
+    pikachu_world.render()
+    update_canvas()
+
+open_canvas(1137,800)
+create_world()
+
+while running:
+    handle_events()
+    update_world()
+    render_world()
+close_canvas()
