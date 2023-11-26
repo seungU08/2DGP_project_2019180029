d0e36514c22003eafe70ee101741d1bbd34bbde0
SWnotebook <sungwoo5957@naver.com>
2023-11-27 00:15:34 +0900

    퍼즈모드 생성 (임시)

---
+++ pause_mode.py
@@ -0,0 +1,40 @@
+from pico2d import *
+
+import game_framework
+import pikachu_world
+import play_mode
+
+
+def handle_events():
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.pop_mode(play_mode)
+
+
+def init():
+
+    pass
+
+def finish():
+    pass
+
+def update():
+    pikachu_world.update()
+    pass
+
+def draw():
+    clear_canvas()
+
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
--- play_mode.py
+++ play_mode.py
@@ -2,6 +2,7 @@ from pico2d import *
 
 import game_framework
 import pikachu_world
+import pause_mode
 import random
 from beach import Beach
 from monster_ball import Monster_ball
@@ -18,6 +19,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.pop_mode(pause_mode)
         else:
             pikachu.handle_event(event)
             pikachu_2.handle_event(event)
